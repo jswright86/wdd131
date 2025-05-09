7769add0c4a8f2dba4f431449f5d150ff74f6ace
jswright86 <jwright9@byupathway.edu>
2025-05-09 02:30:30 -0500

    commit

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,10 +1,8 @@
 let year = document.querySelector("#year");
 let today = new Date();
-
 year.innerHTML = `<span class="highlight">${today.getFullYear()}</span>`;
 let oLastModif = new Date(document.lastModified);
 let nLastModif = new Date(document.lastModified).getTime();
-
 lastModified.innerHTML = `Last Modification: <span class="highlight">${oLastModif.toLocaleDateString("en-US", {
   year: "numeric",
   month: "2-digit",
