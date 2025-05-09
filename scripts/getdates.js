667d60fe79dd5c5bc03bd02b97944c190cf34a0b
jswright86 <jwright9@byupathway.edu>
2025-05-09 01:04:52 -0500

    Refactor date handling and display last modification details

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,2 +1,16 @@
-const year = document.querySelector("#year");
+let year = document.querySelector("#year");
+let today = new Date();
 
+year.innerHTML = `<span class="highlight">${today.getFullYear()}</span>`;
+let oLastModif = new Date(document.lastModified);
+let nLastModif = new Date(document.lastModified).getTime();
+
+lastModified.innerHTML = `Last Modification: <span class="highlight">${oLastModif.toLocaleDateString("en-US", {
+  year: "numeric",
+  month: "2-digit",
+  day: "2-digit",
+})}</span> ${oLastModif.toLocaleTimeString("en-US", {
+  hour: "2-digit",
+  minute: "2-digit",
+  second: "2-digit",
+})}`;
